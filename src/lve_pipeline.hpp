1a8cd895b386299b0be9d7b275f7c5da37fb9b79
Rshyl1023 <wujingmo1023@gmail.com>
2026-07-30 00:35:04 +0800

    [fix] 修复因复制管线配置信息可能造成的悬空指针问题

--- src/lve_pipeline.hpp
+++ src/lve_pipeline.hpp
@@ -9,7 +9,6 @@
 struct PipelineConfigInfo {
     VkViewport viewport;
     VkRect2D scissor;
-    VkPipelineViewportStateCreateInfo viewportInfo;
     VkPipelineInputAssemblyStateCreateInfo inputAssemblyInfo;
     VkPipelineRasterizationStateCreateInfo rasterizationInfo;
     VkPipelineMultisampleStateCreateInfo multisampleInfo;
