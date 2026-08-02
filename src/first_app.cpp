1db8c5029068b73d6944f53852d7820f0b580415
Rshyl1023 <wujingmo1023@gmail.com>
2026-08-02 23:47:21 +0800

    [feat] 实现命令缓冲区录制与逐帧呈现流程

--- src/first_app.cpp
+++ src/first_app.cpp
@@ -1,6 +1,7 @@
 #include "first_app.h"
 
 #include <stdexcept>
+#include <array>
 
 namespace lve {
     FirstApp::FirstApp() {
@@ -16,7 +17,9 @@ namespace lve {
     void FirstApp::run() {
         while (!lveWindow.shouldClose()) {
             glfwPollEvents(); // 只要窗口没关闭 就处理窗口事件
+            drawFrame();
         }
+        vkDeviceWaitIdle(lveDevice.device());
     }
 
     void FirstApp::createPipelineLayout() {
@@ -42,4 +45,71 @@ namespace lve {
             "shaders/simple_shader.vert.spv",
             "shaders/simple_shader.frag.spv");
     }
+
+    void FirstApp::createCommandBuffers() { // 创建帧缓冲区
+        commandBuffers.resize(lveSwapChain.imageCount()); // 设置为与交换链数量一样
+
+        VkCommandBufferAllocateInfo allocInfo{}; // 分配命令缓冲区
+        allocInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_ALLOCATE_INFO;
+        allocInfo.level = VK_COMMAND_BUFFER_LEVEL_PRIMARY;  // 设置为主要命令缓冲区
+        allocInfo.commandPool = lveDevice.getCommandPool(); // 获取设备中命令池
+        allocInfo.commandBufferCount = static_cast<uint32_t>(commandBuffers.size());
+
+        if (vkAllocateCommandBuffers(lveDevice.device(), &allocInfo, commandBuffers.data()) != VK_SUCCESS) {
+            throw std::runtime_error("failed to allocate command buffers!");
+        }
+
+        // 为每个缓冲区记录绘制命令
+        for (int i = 0; i < commandBuffers.size(); i++) {
+            VkCommandBufferBeginInfo beginInfo{};
+            beginInfo.sType = VK_STRUCTURE_TYPE_COMMAND_BUFFER_BEGIN_INFO;
+
+            if (vkBeginCommandBuffer(commandBuffers[i], &beginInfo) != VK_SUCCESS) {
+                throw std::runtime_error("failed to begin recording command buffer!");
+            }
+
+            VkRenderPassBeginInfo renderPassInfo{};
+            renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
+            renderPassInfo.renderPass = lveSwapChain.getRenderPass();
+            renderPassInfo.framebuffer = lveSwapChain.getFrameBuffer(i);
+
+            // 设置渲染区域
+            renderPassInfo.renderArea.offset = { 0, 0 };
+            renderPassInfo.renderArea.extent = lveSwapChain.getSwapChainExtent(); // 防止交换链大小比屏幕还大的情况🤔
+
+            std::array<VkClearValue, 2> clearValues{};
+            clearValues[0].color = {0.1f, 0.1f, 0.1f, 1.0f};
+            clearValues[1].depthStencil = {1.0f, 0};
+            renderPassInfo.clearValueCount = static_cast<uint32_t>(clearValues.size());
+            renderPassInfo.pClearValues = clearValues.data();
+
+            // 记录命令
+            vkCmdBeginRenderPass(commandBuffers[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE); // 后续命令直接写入命令缓冲区 and 不使用次级命令缓冲区
+
+            lvePipeline->bind(commandBuffers[i]);
+            vkCmdDraw(commandBuffers[i], 3, 1, 0, 0);
+
+            vkCmdEndRenderPass(commandBuffers[i]); // 结束渲染通道
+            if (vkEndCommandBuffer(commandBuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("failed to end recording command buffer!");
+
+            }
+        }
+    }
+
+    void FirstApp::drawFrame() {
+        uint32_t imageIndex = 0;
+        auto result = lveSwapChain.acquireNextImage(&imageIndex); // 获取下一帧索引
+
+        if (result != VK_SUCCESS && result != VK_SUBOPTIMAL_KHR) {
+            throw std::runtime_error("failed to acquire swap chain image!");
+        }
+
+        result = lveSwapChain.submitCommandBuffers(&commandBuffers[imageIndex], &imageIndex);
+        if (result != VK_SUCCESS) {
+            throw std::runtime_error("failed to present swap chain image!");
+
+        }
+    }
+
 }
